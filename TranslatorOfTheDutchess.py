843ac59cc1d4554a1f77e097d031690e419486e3
WaYnE041 <wayneekeh@gmail.com>
2019-01-26 17:12:45 -0500

    Update TranslatorOfTheDutchess.py

--- TranslatorOfTheDutchess.py
+++ TranslatorOfTheDutchess.py
@@ -1,7 +1,45 @@
 def dutchess(input):
     dutchess_tongue = {
-        'you': 'thee',
-        'your': 'thou'
+        'you': 'thou',
+        'your': 'thy',
+        'yours' : 'thine',
+        'are' : 'art',
+        'monitor' : 'portal of light',
+        'bug' : 'Satans Dirty Trick',
+        'code' : 'lightspeak',
+        'computer' : 'light calculator',
+        'science' : 'alchemy',
+        'digital' : 'realm of light',
+        'syntax error' : 'lapse in judgement',
+        'programmer' : 'light alchemist', 
+        'cloud' : 'heavens',
+        'cloud storage' : 'heavens vault',
+        'data' : 'knowledge',
+        'data security' : 'knowledges fortress',
+        'big data' : 'Herculean Knowledge',
+        'debug' : 'exorcise',
+        'programming language' : 'language of light',
+        'compiler' : 'light translator',
+        'bandwidth' : 'light capacity',
+        'binary' : 'language of the saints',
+        'bit' : 'light information',
+        'byte' : 'light information',
+        'program' : 'light device',
+        'database' : 'library of knowledge',
+        'i/o port' : 'light connector',
+        'pixel' : 'portrait of light',
+        'psuedocode' : 'psuedo-lightspeak',
+        'source code' : 'holy lightspeak',
+        'spaghetti code' : 'dirty lightspeak',
+        'string' : 'light sequence',
+        'syntax' : 'light structure',
+        '' : '',
+        
+        
+      
+
+            
+        
     }
 
     output = dutchess_tongue[input]
