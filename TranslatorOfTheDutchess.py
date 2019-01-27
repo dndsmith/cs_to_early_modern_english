98645e377c651c76676bcbb36778f48743aee5b8
Daniel Smith <dnls1297@gmail.com>
2019-01-26 21:42:59 -0500

    Uncommented Polly

--- TranslatorOfTheDutchess.py
+++ TranslatorOfTheDutchess.py
@@ -120,45 +120,44 @@ if __name__ == '__main__':
 
     # Create a client using the credentials and region defined in the [adminuser]
     # section of the AWS credentials file (~/.aws/credentials).
-  #  session = Session(profile_name="adminuser")
-   # polly = session.client("polly")
+    session = Session(profile_name="adminuser")
+    polly = session.client("polly")
 
-    #try:
+    try:
         # Request speech synthesis
-     #   response = polly.synthesize_speech(Text=storeString, OutputFormat="mp3",
-#                                           VoiceId="Joanna")
-    #except (BotoCoreError, ClientError) as error:
+        response = polly.synthesize_speech(Text=storeString, OutputFormat="mp3", VoiceId="Joanna")
+    except (BotoCoreError, ClientError) as error:
         # The service returned an error, exit gracefully
-    #    print(error)
-     #   sys.exit(-1)
+        print(error)
+        sys.exit(-1)
 
     # Access the audio stream from the response
-    #if "AudioStream" in response:
+    if "AudioStream" in response:
         # Note: Closing the stream is important as the service throttles on the
         # number of parallel connections. Here we are using contextlib.closing to
         # ensure the close method of the stream object will be called automatically
         # at the end of the with statement's scope.
-     #   with closing(response["AudioStream"]) as stream:
-      #      output = os.path.join(gettempdir(), "elegantSpeech.mp3")
+        with closing(response["AudioStream"]) as stream:
+            output = os.path.join(gettempdir(), "elegantSpeech.mp3")
 
-       #     try:
+            try:
                 # Open a file for writing the output as a binary stream
-        #        with open(output, "wb") as file:
-         #           file.write(stream.read())
-          #  except IOError as error:
+                with open(output, "wb") as file:
+                    file.write(stream.read())
+            except IOError as error:
                 # Could not write to file, exit gracefully
-        #        print(error)
-         #       sys.exit(-1)
+                print(error)
+                sys.exit(-1)
 
-#    else:
+    else:
         # The response didn't contain audio data, exit gracefully
- #       print("Could not stream audio")
-  #      sys.exit(-1)
+        print("Could not stream audio")
+        sys.exit(-1)
 
     # Play the audio using the platform's default player
-   # if sys.platform == "win32":
-    #    os.startfile(output)
-   # else:
+    if sys.platform == "win32":
+        os.startfile(output)
+    else:
         # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
-    #    opener = "open" if sys.platform == "darwin" else "xdg-open"
-     #   subprocess.call([opener, output])
+        opener = "open" if sys.platform == "darwin" else "xdg-open"
+        subprocess.call([opener, output])
