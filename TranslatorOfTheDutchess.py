503c99c16bb7c14db1ba21d9626bac08a2a7e520
lindafang70 <32524278+lindafang70@users.noreply.github.com>
2019-01-26 21:19:03 -0500

    Update TranslatorOfTheDutchess.py

--- TranslatorOfTheDutchess.py
+++ TranslatorOfTheDutchess.py
@@ -1,12 +1,16 @@
 import boto3
 import json
 import sys
-import re
-from string import punctuation
+from boto3 import Session
+from botocore.exceptions import BotoCoreError, ClientError
+from contextlib import closing
+import subprocess
+from tempfile import gettempdir
+import os
 
 def dutchess(input):
     dutchess_tongue = {
-        'you': 'thou',
+                'you': 'thou',
         'your': 'thy',
         'yours' : 'thine',
         'are' : 'art',
@@ -85,15 +89,16 @@ def dutchess(input):
         'rom' : 'noblemans memory',
         'terminal' : 'ambassador of light',
         'brother' : 'bretheren'
-        
     }
-    if input in dutchess_tongue:
-        return dutchess_tongue[input]
+    if input.lower() in dutchess_tongue:
+        return dutchess_tongue[input.lower()]
 
     return input
 
 if __name__ == '__main__':
     file = sys.argv[1]
+    outfile = open("output.txt", 'w')
+    storeString = ''
 
     # comprehend = boto3.client(service_name='comprehend', region_name='region')
 
@@ -104,8 +109,56 @@ if __name__ == '__main__':
             for word in line.split():
                 #pattern.split(word)
                 in1 = dutchess(word)
-                print(in1)
+                storeString = storeString + ' '+ in1
 
     #print('Calling DetectKeyPhrases')
     #print(json.dumps(comprehend.detect_key_phrases(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
     #print('End of DetectKeyPhrases\n')
+
+    outfile.write(storeString)
+    print(storeString)
+
+    # Create a client using the credentials and region defined in the [adminuser]
+    # section of the AWS credentials file (~/.aws/credentials).
+  #  session = Session(profile_name="adminuser")
+   # polly = session.client("polly")
+
+    #try:
+        # Request speech synthesis
+     #   response = polly.synthesize_speech(Text=storeString, OutputFormat="mp3",
+#                                           VoiceId="Joanna")
+    #except (BotoCoreError, ClientError) as error:
+        # The service returned an error, exit gracefully
+    #    print(error)
+     #   sys.exit(-1)
+
+    # Access the audio stream from the response
+    #if "AudioStream" in response:
+        # Note: Closing the stream is important as the service throttles on the
+        # number of parallel connections. Here we are using contextlib.closing to
+        # ensure the close method of the stream object will be called automatically
+        # at the end of the with statement's scope.
+     #   with closing(response["AudioStream"]) as stream:
+      #      output = os.path.join(gettempdir(), "elegantSpeech.mp3")
+
+       #     try:
+                # Open a file for writing the output as a binary stream
+        #        with open(output, "wb") as file:
+         #           file.write(stream.read())
+          #  except IOError as error:
+                # Could not write to file, exit gracefully
+        #        print(error)
+         #       sys.exit(-1)
+
+#    else:
+        # The response didn't contain audio data, exit gracefully
+ #       print("Could not stream audio")
+  #      sys.exit(-1)
+
+    # Play the audio using the platform's default player
+   # if sys.platform == "win32":
+    #    os.startfile(output)
+   # else:
+        # the following works on Mac and Linux. (Darwin = mac, xdg-open = linux).
+    #    opener = "open" if sys.platform == "darwin" else "xdg-open"
+     #   subprocess.call([opener, output])
